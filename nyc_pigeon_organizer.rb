3a0b0e899fe4d95ce39f1ae7aaaf62635caf99df
Summer Rizzo <srainrizzo@gmail.com>
2019-12-08 01:33:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,16 +11,16 @@ def nyc_pigeon_organizer(data)
     inner_h = {}
 
     #get the colors of the pigeon
-    color_value = data[:color].find_all{|color, names| names.include?(name)}.collect{|color, names| color}
-    inner_h[:color] = color_value.to_s
+    color_value = data[:color].find_all{|color, names| names.include?(name)}.collect{|color, names| color.to_s}
+    inner_h[:color] = color_value.
 
     #get the gender of the pigeon
-    gender_value = data[:gender].find_all{|gender, names| names.include?(name)}.collect{|gender, names| gender}
-    inner_h[:gender] = gender_value.to_s
+    gender_value = data[:gender].find_all{|gender, names| names.include?(name)}.collect{|gender, names| gender.to_s}
+    inner_h[:gender] = gender_value.
 
     #get the life of the pigeon
-    lives_value = data[:lives].find_all{|lives, names| names.include?(name)}.collect{|lives, names| lives}
-    inner_h[:lives] = lives_value.to_s
+    lives_value = data[:lives].find_all{|lives, names| names.include?(name)}.collect{|lives, names| lives.to_s}
+    inner_h[:lives] = lives_value.
 
     #add it all to the hash! yahoo!
     h[name] = inner_h
